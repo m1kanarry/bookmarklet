f7711ca7783e45cb777f70c1adc66823e1707c15
m1kanarry <m1kan.dqx@gmail.com>
2018-05-12 01:04:17 +0900

    add new file

---
+++ bookmarklet.js
@@ -0,0 +1,6 @@
+javascript:
+(function() {
+  var s = document.createElement("script");
+  s.src = "https://github.com/m1kanarry/bookmarklet/blob/master/sub.js";
+  document.body.appendChild(s);
+})();
