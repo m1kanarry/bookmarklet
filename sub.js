3b91fbbca741ebb121836967ca0814999cdbb1c3
m1kanarry <m1kan.dqx@gmail.com>
2018-05-12 00:30:07 +0900

    add new file

---
+++ sub.js
@@ -0,0 +1,26 @@
+/**
+ *
+ */
+(function() {
+  "use strict";
+
+  alert("hello");
+
+  getUserId();
+
+  var character = [];
+  var friends = [];
+
+  function getUserId() {
+    var myCharacterImg = document.getElementById("myCharacterImg");
+    console.log(myCharacterImg);
+  }
+
+})();
+
+/**
+http://hiroba.dqx.jp/sc/character/468908142389/friendlist/
+
+http://hiroba.dqx.jp/sc/character/29573377729/friendlist/
+
+*/
