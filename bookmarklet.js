efe1420eb2736b18bfb48ea9e5d1c2ba05910a5c
m1kanarry <39195700+m1kanarry@users.noreply.github.com>
2018-05-12 11:26:33 +0900

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,6 +1,5 @@
 (function(d) {
   var s = d.createElement("script");
   s.src = "https://raw.githubusercontent.com/m1kanarry/bookmarklet/master/bookmarklet.js";
-  //d.body.appendChild(s);
   d.getElementByTagName("head")[0].appendChild(s);
 })(document);
