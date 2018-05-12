178764d6e933268fd7b333b0bf6bfe54a1dc6ba3
m1kanarry <39195700+m1kanarry@users.noreply.github.com>
2018-05-12 12:20:05 +0900

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,5 +1,14 @@
 (function(d) {
   var s = d.createElement("script");
-  s.src = "https://raw.githubusercontent.com/m1kanarry/bookmarklet/master/bookmarklet.js";
+  /* 
+   * https://cdn.rawgit.com/m1kanarry/bookmarklet/master/bookmarklet.js
+   * https://raw.githubusercontent.com/m1kanarry/bookmarklet/master/bookmarklet.js
+   * http://rawgit.com/m1kanarry/bookmarklet/master/bookmarklet.js
+   * https://m1kanarry.github.io/bookmarklet/bookmarklet.js
+   */
+  s.type = "text/javascript";
+  s.charset= "UTF-8";
+  s.language= "javascript";
+  s.src = "http://rawgit.com/m1kanarry/bookmarklet/master/bookmarklet.js";
   d.getElementsByTagName("head")[0].appendChild(s);
 })(document);
