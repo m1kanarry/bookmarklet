861a851a99a20023b86409b95fbafb7b15c86a4f
m1kanarry <m1kan.dqx@gmail.com>
2018-05-12 17:46:53 +0900

    a

--- sub.js
+++ sub.js
@@ -4,37 +4,33 @@
 (function() {
     "use strict";
 
-    console.log("!!!");
-
     var i = 0;
     var userId = getUserId();
 
-    console.log(i++);
-
     var url = "http://hiroba.dqx.jp/sc/character/" + userId + "/friendlist/";
 
-    var readData = "初期化";
-
-    console.log(i++);
-    $("#read").load(url, function(data, status) {
-        if (status == "success") {
-            console.log("success:" + url);
-            readData = data;
-        } else {
-            console.log("error:" + url);
-        }
-        console.log("?:" + url);
-    });
-
-    console.log(i++);
-
-    console.log(readData);
+    var friends = getFriends();
+    console.log(friends);
 
     function getUserId() {
         var myCharacterImg = document.getElementById("myCharacterImg");
         var src = myCharacterImg.getElementsByTagName("img")[0].src;
         var userId = src.split("/")[5];
         return userId;
+    }
+
+    function getFriends() {
+        var friends;
+        $("#read").load(url, function(data, status) {
+            if (status == "success") {
+                console.log("success:" + url);
+                friends = data;
+            } else {
+                console.log("error:" + url);
+            }
+            console.log("?:" + url);
+        });
+        return friends;
     }
 
 })();
