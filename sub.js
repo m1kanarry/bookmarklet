7052d0ece64af4497c17d1aa4ab43baea8eb3acb
m1kanarry <m1kan.dqx@gmail.com>
2018-05-12 17:15:39 +0900

    h

--- sub.js
+++ sub.js
@@ -2,38 +2,39 @@
  *
  */
 (function() {
-  "use strict";
+    "use strict";
 
-  console.log("!!!");
+    console.log("!!!");
 
-  var i = 0;
-  var userId = getUserId();
+    var i = 0;
+    var userId = getUserId();
 
-  console.log(i++);
+    console.log(i++);
 
-  var url = "http://hiroba.dqx.jp/sc/character/" + userId + "/friendlist/";
+    var url = "http://hiroba.dqx.jp/sc/character/" + userId + "/friendlist/";
 
-  var readData = "初期化";
+    var readData = "初期化";
 
-  console.log(i++);
-  $("#read").load(url, function(data, status) {
-    if (status == "success") {
-      readData = data;
-    } else {
-      console.log("error:" + url);
+    console.log(i++);
+    $("#read").load(url, function(data, status) {
+        if (status == "success") {
+            console.log("success:" + url);
+            readData = data;
+        } else {
+            console.log("error:" + url);
+        }
+    });
+
+    console.log(i++);
+
+    console.log(readData);
+
+    function getUserId() {
+        var myCharacterImg = document.getElementById("myCharacterImg");
+        var src = myCharacterImg.getElementsByTagName("img")[0].src;
+        var userId = src.split("/")[5];
+        return userId;
     }
-  });
-
-  console.log(i++);
-
-  console.log(readData);
-
-  function getUserId() {
-    var myCharacterImg = document.getElementById("myCharacterImg");
-    var src = myCharacterImg.getElementsByTagName("img")[0].src;
-    var userId = src.split("/")[5];
-    return userId;
-  }
 
 })();
 
