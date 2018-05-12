be8f715dc0128cc2b8ad936cbf39a6eb96e24378
m1kanarry <m1kan.dqx@gmail.com>
2018-05-12 16:38:22 +0900

    a

--- sub.js
+++ sub.js
@@ -4,6 +4,8 @@
 (function() {
   "use strict";
 
+  useJQuery();
+
   var userId = getUserId();
   var url = "http://hiroba.dqx.jp/sc/character/" + userId + "/friendlist/";
 
@@ -24,6 +26,15 @@
     var src = myCharacterImg.getElementsByTagName("img")[0].src;
     var userId = src.split("/")[5];
     return userId;
+  }
+
+  function useJQuery() {
+    var script = document.createElement("script");
+    script.src = "//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js";
+    script.onload = function() {
+      func(jQuery.noConflict(true));
+    };
+    document.body.appendChild(script);
   }
 
 })();
