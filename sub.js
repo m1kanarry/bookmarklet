d9370fc5b9e485249b8f488496e049eb3b177044
m1kanarry <m1kan.dqx@gmail.com>
2018-05-13 18:42:23 +0900

    a

--- sub.js
+++ sub.js
@@ -17,12 +17,21 @@
     }
 
     var url = "http://hiroba.dqx.jp/sc/character/" + userId + "/friendlist/page/";
-    var friendAll;
+    var defferdObj = [];
+    for(var i = 0; $(".last a").attr("href"); i++) {
+        defferdObj.push($.get(url + i));
+    }
 
-    $.when(getFriendAll())
+    $.when(null, defferdObj)
     .done(function() {
-        console.log("finished get friendAll");
-        console.log(friendAll);
+        var friends = [];
+        Array.prototype.foreach.call(arguments, function(argument) {
+            friends.push(getFriends(argument));
+        });
+        console.log("success");
+        console.log(friends);
+    }).fail(function() {
+        console.log("failed");
     });
 
     function getUserId() {
@@ -32,44 +41,20 @@
         return userId;
     }
 
-    function getFriendAll(friendAll = [], count = 0) {
-        // if (typeof friends === "undefined") friends = [];
-        // if (typeof count === "undefined") count = 0;
-
-        if (count === 20) return friendAll;
-
-        console.log(url + count);
-        console.log(friendAll);
-
-        $.get(url + count)
-        .done(function(data) {
-            var friends = getFriends($(data).find("box_charaInfo"));
-            friendAll.push(...friends);
-            console.log("success:" + count);
-            if (friends.length === 10) {
-                friends = getFriendAll(friendAll, ++count);
-            } else {
-                return friendAll;
-            }
-        }).fail(function() {
-            console.log("failed:" + count);
-            return friendAll;
-        });
-    }
-
-    function getFriends(characterInfos) {
+    function getFriends(data) {
+        var characterInfos = $(data).find("box_charaInfo");
         console.log("characterInfos");
         console.log(characterInfos);
         var friends = [];
         Array.prototype.forEach.call(characterInfos, function(characterInfo) {
             console.log("☆");
-            var dd = characterInfo.getElementsByTagName("dd");
+            var dd = characterInfo.find("dd");
             friends.push({
-                name: dd[0].getElementsByTagName("a")[0],
-                userId: dd[0].getElementsByTagName("a")[0].getAttribute("href").split("/")[3],
+                name: dd[0].find("a")[0],
+                userId: dd[0].find("a")[0].attr("href").split("/")[3],
                 characterId: dd[1].replace("： ", ""),
-                registrationDate: characterInfo.getElementsByClassName("txt_listeddate")[0].replace("登録日：", ""),
-                memo: characterInfo.getElementsByTagName("memo")[0]
+                registrationDate: characterInfo.find(".txt_listeddate")[0].replace("登録日：", ""),
+                memo: characterInfo.find(".memo")[0]
             });
         });
         console.log(friends);
