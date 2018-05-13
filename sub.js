dda7d0320017fdee6b8038d4a26f4d62b3ffec08
m1kanarry <m1kan.dqx@gmail.com>
2018-05-13 16:17:32 +0900

    a

--- sub.js
+++ sub.js
@@ -17,7 +17,7 @@
     }
 
     var url = "http://hiroba.dqx.jp/sc/character/" + userId + "/friendlist/page/";
-    var friendAll = getFriends();
+    var friendAll = getFriendAll();
     console.log(friendAll);
 
     function getUserId() {
@@ -27,28 +27,43 @@
         return userId;
     }
 
-    function getFriends(friends = [], count = 0) {
+    function getFriendAll(friendAll = [], count = 0) {
         // if (typeof friends === 'undefined') friends = [];
         // if (typeof count === 'undefined') count = 0;
 
-        if (count === 20) return friends;
+        if (count === 20) return friendAll;
 
         console.log(url + count);
-        console.log(friends);
+        console.log(friendAll);
 
         $.get(url + count)
         .done(function(data) {
-            if (data === null) {
-                console.log("nodata:" + count);
-            } else {
-                //friends.push(data);
-                friends.push(...[count]);
-                console.log("success:" + count);
-                friends = getFriends(friends, ++count);
+            var friends = getFriends(data);
+            friendAll.push(...friends);
+            console.log("success:" + count);
+            if (friends.length === 10) {
+                friends = getFriendAll(friendAll, ++count);
             }
         }).fail(function() {
             console.log("failed:" + count);
         });
+        return friendAll;
+    }
+
+    function getFriends(data) {
+        var characterInfos = data.getElementsByClassName("box_charaInfo tile");
+        var friends = [];
+        for (var characterInfo of characterInfos) {
+            var dd = characterInfo.getElementsByTagName("dd");
+            friends.push({
+                name: dd[0].getElementsByTagName("a")[0],
+                userId: dd[0].getElementsByTagName("a")[0].getAttribute("href").split("/")[3],
+                characterId: dd[1].replace("： ", ""),
+                registrationDate: characterInfo.getElementsByClassName("txt_listeddate")[0].replace("登録日：", ""),
+                memo: characterInfo.getElementsByTagName("memo")[0]
+            });
+        }
+        console.log(friends);
         return friends;
     }
 
